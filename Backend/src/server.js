ab49438b59df3669c48e3f32b9520a982e11b7b9
hpaanonlineuniversity <hpaanonlineuniversity@gmail.com>
2025-10-16 11:34:38 +0630

    fix duplicate listen

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -52,15 +52,10 @@ app.get('/', (req, res) => {
 });
 
 connectDB().then(() => {
-    app.listen(PORT, () => {
-      
-        // '0.0.0.0' ကို explicitly bind လုပ်ပါ
       app.listen(PORT, '0.0.0.0', () => {
           console.log(`✅ Server is running on http://0.0.0.0:${PORT}`);
           console.log(`✅ Accessible via http://localhost:${PORT}`);
       }); 
-
-  });
 }).catch((err) => {
   console.error('Database connection failed:', err);
   process.exit(1); // Exit process with failure
